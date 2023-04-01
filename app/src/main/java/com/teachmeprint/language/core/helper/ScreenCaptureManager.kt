f9b681b866a38a950337c69034e9dedeefc4a8ff
chamsilva <charles.m.silva@mercadolivre.com>
2023-04-01 13:34:57 -0300

    Update.

--- app/src/main/java/com/teachmeprint/language/core/helper/ScreenCaptureManager.kt
+++ app/src/main/java/com/teachmeprint/language/core/helper/ScreenCaptureManager.kt
@@ -70,16 +70,21 @@ class ScreenCaptureManager @Inject constructor(
         val buffer = planes[0].buffer
         val pixelStride = planes[0].pixelStride
         val rowStride = planes[0].rowStride
+
         val cropWidth = cropRect.width()
         val cropHeight = cropRect.height()
+
         val cropOffsetX = cropRect.left * pixelStride
         val cropOffsetY = cropRect.top * rowStride
         val rowPadding = rowStride - cropWidth * pixelStride
+
         val croppedBitmap = Bitmap.createBitmap(
             cropWidth, cropHeight, Bitmap.Config.ARGB_8888
         )
+
         val croppedBuffer = ByteBuffer.allocate(cropWidth * cropHeight * 4)
         buffer.position(cropOffsetY + cropOffsetX)
+
         for (y in 0 until cropHeight) {
             for (x in 0 until cropWidth) {
                 val pixel = buffer.int
@@ -87,6 +92,7 @@ class ScreenCaptureManager @Inject constructor(
             }
             buffer.position(buffer.position() + rowPadding)
         }
+
         croppedBuffer.rewind()
         croppedBitmap.copyPixelsFromBuffer(croppedBuffer)
         return croppedBitmap
