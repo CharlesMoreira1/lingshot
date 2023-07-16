8eb0e349f78a930252b9a750fe73fb4d9403856b
chamsilva <charles.m.silva@mercadolivre.com>
2023-07-16 16:39:39 -0300

    Add secret keys ci.

--- plugins/src/main/java/AndroidAdmobPlugin.kt
+++ plugins/src/main/java/AndroidAdmobPlugin.kt
@@ -18,11 +18,9 @@ class AndroidAdmobPlugin : Plugin<Project> {
                 buildTypes {
                     release {
                         resValue("string", "ADMOB_APP_ID", KeyHelper.getValue("ADMOB_APP_ID_RELEASE"))
-                        buildConfigField("String", "ADMOB_INTERSTITIAL_ID", KeyHelper.getValue("ADMOB_INTERSTITIAL_ID_RELEASE"))
                     }
                     debug {
                         resValue("string", "ADMOB_APP_ID", KeyHelper.getValue("ADMOB_APP_ID_DEBUG"))
-                        buildConfigField("String", "ADMOB_INTERSTITIAL_ID", KeyHelper.getValue("ADMOB_INTERSTITIAL_ID_DEBUG"))
                     }
                 }
             }
